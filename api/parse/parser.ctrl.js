a27e24be801f13ddee1dd86e8a953c7c77646665
KimPinot <sohuyn43@gmail.com>
2019-11-03 01:33:46 +0900

    🔨 쿼리 검사 기능 추가
    isNaN() 을 사용해 Number로 변환된 쿼리가 숫자가 아니라면 400 에러를 주는 기능을 추가했습니다.

--- api/parse/parser.ctrl.js
+++ api/parse/parser.ctrl.js
@@ -1,19 +1,17 @@
 /*
  * 시간표 불러오기
- * 들어오는 쿼리 : [post] api/parse/
- * {
- *     "dataGrade" : number,
- *     "dataClass" : number
- * }
+ * 들어오는 쿼리 예시 : localhost:4001/api/parse/?dataGrade=1&dataClass=4
+ * 기록되는 쿼리 : [Object: null prototype] { dataGrade: '1', dataClass: '4' }
  */
 
-// TODO : typeof 체크해서 맞으면 응답 주고, 틀리면 400 에러 뜨게 하는 기능 만들기
 exports.getData = async (ctx) => {
-    if (typeof(dataGrade) === Number && typeof(dataClass) === Number) {
-        ctx.body(dataGrade + "|" + dataClass);
-    } else {
-
+    const schema = ctx.query;
+    if (isNaN(Number(schema.dataGrade)) || isNaN(Number(schema.dataClass))) {
+        ctx.status = 400; // Bad requset
+        return;
     }
+
+    ctx.body = "hello?";
 };
 
 exports.test = async (ctx) => {

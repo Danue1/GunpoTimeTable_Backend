2c1b68b23a2cabcd7220bbc253ec5b443ac773ba
KimPinot <sohuyn43@gmail.com>
2019-11-04 23:45:00 +0900

    🤔 코드 순서 변경
    다른 기능은 없지만, 코드의 가독성을 위해 순서를 바꿨습니다.

--- src/api/parse/index.js
+++ src/api/parse/index.js
@@ -2,13 +2,13 @@ const Router = require('koa-router');
 const parse = new Router();
 const parseCtrl = require('./parser.ctrl');
 
-// Get All Table Data
-parse.get('/getidx', parseCtrl.getidx);
-
 // Add New Table Data
 parse.post('/insert', parseCtrl.insertData);
 
 // Get All Table Data
 parse.get('/get', parseCtrl.getData);
+
+// Get Recent Table ID
+parse.get('/getidx', parseCtrl.getidx);
 
 module.exports = parse;
